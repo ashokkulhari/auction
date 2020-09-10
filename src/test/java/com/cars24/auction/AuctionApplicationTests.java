618059261106f447e405554d577b7b28a5d9f83c
ashokkulhari <ashok.kulhari@gmail.com>
2020-09-10 16:32:48 +0530

    first commit

---
+++ src/test/java/com/cars24/auction/AuctionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cars24.auction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuctionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
